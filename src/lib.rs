c0403ae5d1529a59e075062c19ec25c620a5f616
Simon Golz <simon.golz16@gmail.com>
2022-10-10 18:13:23 +0200

    Use modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-pub fn requires_subtraction(number: &u16) -> bool {
-    let operator: [u16; 5] = [1, 5, 10, 50, 100];
-    let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
+pub mod checks {
+    pub fn requires_subtraction(number: &u16) -> bool {
+        let operator: [u16; 5] = [1, 5, 10, 50, 100];
+        let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
 
-    for i in operator.iter() {
-        for j in round.iter() {
-            if number + *i == *j {
-                return false;
+        for i in operator.iter() {
+            for j in round.iter() {
+                if number + *i == *j {
+                    return false;
+                };
             };
         };
-    };
 
-    return true;
+        return true;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use vita_brevis::checks::{requires_subtraction};
 
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
@@ -9,21 +10,6 @@ fn main() {
     } else {
         resolve_addition(&number);
     };
-}
-
-fn requires_subtraction(number: &u16) -> bool {
-    let operator: [u16; 5] = [1, 5, 10, 50, 100];
-    let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
-
-    for i in operator.iter() {
-        for j in round.iter() {
-            if number + *i == *j {
-                return false;
-            };
-        };
-    };
-
-    return true;
 }
 
 fn resolve_subtraction(number: &u16) {
