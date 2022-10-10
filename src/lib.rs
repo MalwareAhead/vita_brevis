fdceb91e443a42b4a25121d928030faafd0ed92e
Simon Golz <simon@Simons-MacBook-Air.local>
2022-10-10 18:00:22 +0200

    Outsource exception check

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub fn requires_subtraction(number: &u16) -> bool {
+    let operator: [u16; 5] = [1, 5, 10, 50, 100];
+    let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
+
+    for i in operator.iter() {
+        for j in round.iter() {
+            if number + *i == *j {
+                return false;
+            };
+        };
+    };
+
+    return true;
+}
