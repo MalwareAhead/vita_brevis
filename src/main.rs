12b1f4d6b2f80fcda5406546d122fd4ab9daad56
Simon Golz <simon@Simons-MacBook-Air.local>
2022-10-10 17:55:19 +0200

    Give more functional structure

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,34 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
-    // let number: &u16 = &args[1].parse().unwrap();
+    let number: &u16 = &args[1].parse().unwrap();
 
-    let operator: [u16; 5] = [1, 5, 10, 50, 100];
-    let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
-
-    for number in 1..=1000 {
-        for i in operator.iter() {
-            for j in round.iter() {
-                if number + *i == *j {
-                    resolve_number_with_exception(&number);
-                };
-            };
-        };
-        resolve_number_as_usual(&number);
+    if requires_subtraction(&number) {
+        resolve_subtraction(&number);
+    } else {
+        resolve_addition(&number);
     };
 }
 
-fn resolve_number_as_usual(number: &u16) {
+fn requires_subtraction(number: &u16) -> bool {
+    let operator: [u16; 5] = [1, 5, 10, 50, 100];
+    let round: [u16; 6] = [5, 10, 50, 100, 500, 1000];
+
+    for i in operator.iter() {
+        for j in round.iter() {
+            if number + *i == *j {
+                return false;
+            };
+        };
+    };
+
+    return true;
+}
+
+fn resolve_subtraction(number: &u16) {
     println!("Usual number: {}", &number);
 }
 
-fn resolve_number_with_exception(number: &u16) {
+fn resolve_addition(number: &u16) {
     println!("Exceptional number: {}", &number);
 }
